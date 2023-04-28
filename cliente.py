b90bf634b80f4eacaf7e58693460983208dfa9c7
Ana Márcia <70349481+anamarcialima@users.noreply.github.com>
2023-04-28 16:59:35 -0300

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -1,15 +1,29 @@
 import socket
 import random
+import string
+from time import sleep
 
 HOST = '127.0.0.1'
 PORT = 5000
 
-inteiroAleatorio = random.randint(0, 999999999999999999999999999999) # gera um número aleatório de até 30 casas
+while True:
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.sendall(inteiroAleatorio.to_bytes(inteiroAleatorio.bit_length() + 7 // 8, byteorder='big')) # converte o número aleatório gerado em bytes para mandar para o servidor. encode converte string. inteiroAleatorio.bit_length() + 7 // 8 faz o calculo do número de bytes necessários para aquele número aleatório
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
 
-data = s.recv(1024)
+    inteiroAleatorio = random.randint(0, 999999999999999999999999999999) # gera um número aleatório de até 30 casas
+    inteiroAleatorioEmBytes = inteiroAleatorio.to_bytes(inteiroAleatorio.bit_length() + 7 // 8, byteorder='big') # converte o número aleatório gerado em bytes | (inteiroAleatorio.bit_length() + 7 // 8) faz o calculo do número de bytes necessários para aquele número aleatório
+    s.sendall(inteiroAleatorioEmBytes)  # manda número aletório convertido em bytes para o servidor 
+    print('Inteiro aleatório gerado', inteiroAleatorio)
 
+    resposta = s.recv(1024) # recebe resposta do servidor 
+    print("Resposta", resposta)
+
+    s.sendall(resposta) # devolve valor recebido do servidor
+
+    print("FIM")
+
+    s.close() # fecha a conexão
+
+    sleep(10)
 
--- servidor.py
+++ servidor.py
@@ -11,31 +11,21 @@ s.listen()
 
 print ('Aguardando conexão de um cliente')
 
-conn, ender = s.accept()
+conn, ender = s.accept() # aceita a conexão com cliente
 print ('Conectado em ', ender)
 
-data = conn.recv(1024) # recebe número aleatório em bytes 
-dataConvertido = int.from_bytes(data, byteorder='big') # número em bytes covertido para inteiro
-print("Número aleatório recebido:", dataConvertido)
+inteiroAleatorio = conn.recv(1024) # recebe número aleatório em bytes 
+inteiroAleatorioEmBytes = int.from_bytes(inteiroAleatorio, byteorder='big') # número em bytes covertido para inteiro
+print("Número aleatório recebido:", inteiroAleatorioEmBytes)
 
-tamanhoData = len(str(dataConvertido))
-
-if tamanhoData > 10:
-    stringAleatoria = ''.join(random.choices(string.ascii_uppercase, k = len(str(dataConvertido)))) # gerar string aleatória
+if len(str(inteiroAleatorioEmBytes)) > 10:
+    stringAleatoria = ''.join(random.choices(string.ascii_uppercase, k = len(str(inteiroAleatorioEmBytes)))) # gerar string aleatória
     print("String aleatória gerada: ", stringAleatoria)
     
-    s.sendall(str.encode(stringAleatoria)) # enviar para o cliente
+    conn.sendall(str.encode(stringAleatoria)) # enviar string aleatória para o cliente
     
 else:
-    if dataConvertido % 2 == 0:
-        print("PAR")
-        #mandar para cliente
+    if inteiroAleatorioEmBytes % 2 == 0:
+        conn.sendall(str.encode("PAR"))
     else:
-        print("IMPAR")
-        #mandar para cliente
-
-if not data:
-    print ('Fechando a conexão')
-    conn.close()
-        
-conn.sendall(data)
+        conn.sendall(str.encode("IMPAR"))
